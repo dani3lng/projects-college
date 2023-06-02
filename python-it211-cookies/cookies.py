468655d9a8aa7247c5a3c6defe7ef46647891d28
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2023-06-02 01:34:26 -0700

    improved

--- python-it211-cookies/cookies.py
+++ python-it211-cookies/cookies.py
@@ -3,11 +3,15 @@
 # Display the number of cups of each ingredient needed for the specified number
 # of cookies
 
-print ("To bake 100 cookies, you will require\n3 cups of sugar\n1.5 cups of butter\n4 cups of flour") 
+print (f'''To bake 100 cookies, you will require:
+       3 cups of sugar
+       1.5 cups of butter
+       4 cups of flour
+       ''') 
 
 #display question
 num = input("How many cookies would you like to make? ")
-print("You want to bake", num, "cookies")
+print(f"You want to bake {num} cookies")
 
 #perform calculation
 div = float(num)/100
@@ -16,4 +20,9 @@ sugar = div*3
 butter = div*1.5
 flour = div*4
 
-print("Your recipe will require\n", sugar, "cups of sugar\n",butter, "cups of butter\n", flour, "cups of flour")
+print(f'''
+Your recipe will require:
+      {sugar:.3f} cups of sugar 
+      {butter:.3f} cups of butter 
+      {flour:.3f} cups of flour
+      ''')
