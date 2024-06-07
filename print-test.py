aa96c06226447fe956af8ef15099bdb96f86e281
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2024-06-07 08:34:48 -0700

    added print code.

---
+++ print-test.py
@@ -0,0 +1,9 @@
+name = 'Danny'
+email_address = 'danny@gmail.com'
+current_job = 'spaceX'
+dream_job = 'CIA'
+
+print ("My name is", (name))
+print ("My email address is", (email_address))
+print ("My current job is", (current_job))
+print ("My dream job is", (dream_job))
