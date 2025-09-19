6a8ea7678b628cd766ef3a38fc16a15082e58efc
Daniel Ng <dani3l.yng@gmail.com>
2025-09-19 06:56:24 -0700

    updated

--- python/ist820-eventextraction/Exercise B/output_b.py
+++ python/ist820-eventextraction/Exercise B/output_b.py
@@ -4,7 +4,7 @@
 # total 'a program start running' counter
 loga_programstart = 0
 logb_programstart = 0
-# 'write events' counter
+# total 'write events' counter
 loga_write = 0
 logb_write = 0
 # total 'get file/directory status' counter
@@ -18,50 +18,78 @@ loga_programends = 0
 logb_programends = 0
 
 # create list for program names
-program_exec = [] 
-program_write = [] 
-program_getfile = [] 
-program_unlink = [] 
-program_end = [] 
+program_exec_loga = []
+program_exec_logb = []
+program_write_loga = [] 
+program_write_logb = [] 
+program_getfile_loga = [] 
+program_getfile_logb = [] 
+program_unlink_loga = [] 
+program_unlink_logb = [] 
+program_end_loga = [] 
+program_end_logb = [] 
 
 name = input('Name text file: ')
 print('Starting Output A')
 # set file paths for logs
 file_path1 = 'DataSource/LogA.strace'
-# file_path2 = 'DataSource/LogB.strace'
+file_path2 = 'DataSource/LogB.strace'
 # open the log files
 file1=open(file_path1)
-# file2=open(file_path2)
+file2=open(file_path2)
 # read logs content into memory object “lines” 
 lines1=file1.readlines()
-# lines2=file2.readlines()
+lines2=file2.readlines()
 # close the file 
 file1.close()
-# find events in data logs
+file2.close()
+# events to find in data logs
 term1=' execve('
 term2=' write('
 term3=' access('
 term4=' stat('
 term5=' unlinkat('
 term6=' exit_group('
-# find terms in LogA
+
+# find term1 in Logs
 for line in lines1:
       if term1 in line:
             position_a = line.find('"')
             position_b = line.find('"', position_a + 1)
             name = line[position_a + 1 : position_b]
-            program_exec.append(name)
+            program_exec_loga.append(name)
+            programexec_logalen= len(program_exec_loga)
+for line in lines2:
       if term1 in line:
             position_a = line.find('”')
             position_b = line.find('"', position_a + 1)
             name = line[position_a + 1 : position_b]
-            program_exec.append(name) 
+            program_exec_logb.append(name) 
+            programexec_logblen = len(program_exec_logb)
 
-print(program_exec)
-# # create output table
-# print(f"""Event                 LogA    LogB
-# Total number of 'a program starts running':     {loga_readtotal}     {logb_readtotal}
-# Read from a keyboard: {loga_keytotal}     {logb_keytotal}
+# find term2 in Logs
+for line in lines1:
+      if term2 in line:
+            position_a = line.find('"')
+            position_b = line.find('"', position_a + 1)
+            name = line[position_a + 1 : position_b]
+            program_write_loga.append(name)
+            programwrite_logalen = len(program_write_loga)
+for line in lines2:
+      if term2 in line:
+            position_a = line.find('”')
+            position_b = line.find('"', position_a + 1)
+            name = line[position_a + 1 : position_b]
+            program_write_logb.append(name) 
+            programwrite_logblen = len(program_write_logb)
+            
+# find term3 in Logs
+            
+# create output table
+print(f"""Event                                           LogA    LogB
+Total number of 'a program starts running':     {programexec_logalen}     {programexec_logblen}
+Total number of 'write events'                  {programwrite_logalen}    {programwrite_logalen}""")
+
 # Read from pipe:       {loga_pipetotal}     {logb_pipetotal}""", file=open(name, 'a'))
 
 # close the file 
