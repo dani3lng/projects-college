8ee718c00905dac4cce629615272677c6cad0aa7
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2025-09-23 13:32:16 -0700

    updated

--- python/ist820-eventextraction/Exercise A/exercisea.py
+++ python/ist820-eventextraction/Exercise A/exercisea.py
@@ -27,118 +27,158 @@ def log_file(term, term2, term3):
 
 ## Output A
 print('Starting Output A')
-# output total number of 'read' events and each 'read' event
-file_path = 'ist820-eventextraction/DataSource/LogA.strace'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# print read events to console
-term=' read('
-counter_read(term)
-# close the file 
-file1.close()
+user_input = input('Would you like to perform Output A: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # output total number of 'read' events and each 'read' event
+    file_path = 'DataSource/LogA.strace'
+    # open the file
+    file1=open(file_path)
+    # read content into memory object “lines” 
+    lines=file1.readlines()
+    # print read events to console
+    term=' read('
+    counter_read(term)
+    # close the file 
+    file1.close()
+else:
+    pass
 
 ## Output B
 print('Starting Output B')
-# output total number of 'read' events and each 'read' event
-file_path = 'ist820-eventextraction/DataSource/LogB.strace'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# print read events to console
-term=' read('
-counter_read(term)
-# close the file 
-file1.close()
+user_input = input('Would you like to perform Output B: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # output total number of 'read' events and each 'read' event
+    file_path = 'DataSource/LogB.strace'
+    # open the file
+    file1=open(file_path)
+    # read content into memory object “lines” 
+    lines=file1.readlines()
+    # print read events to console
+    term=' read('
+    counter_read(term)
+    # close the file 
+    file1.close()
+else:
+    pass
 
 ## Output C
 print('Starting Output C')
-# output each 'read from keyboard' event
-file_path = 'ist820-eventextraction/DataSource/LogA.strace'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# print read events to console
-term=' read('
-term2='tty'
-log_key(term, term2)
-# close the file 
-file1.close()
+user_input = input('Would you like to perform Output C: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # output each 'read from keyboard' event
+    file_path = 'DataSource/LogA.strace'
+    # open the file
+    file1=open(file_path)
+    # read content into memory object “lines” 
+    lines=file1.readlines()
+    # print read events to console
+    term=' read('
+    term2='tty'
+    log_key(term, term2)
+    # close the file 
+    file1.close()
+else:
+    pass
 
 ## Output D
 print('Starting Output D')
-# output each 'read from keyboard' event
-file_path = 'ist820-eventextraction/DataSource/LogB.strace'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# print read events to console
-term=' read('
-term2='tty'
-log_key(term, term2)
-# close the file 
-file1.close()
+user_input = input('Would you like to perform Output D: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # output each 'read from keyboard' event
+    file_path = 'DataSource/LogB.strace'
+    # open the file
+    file1=open(file_path)
+    # read content into memory object “lines” 
+    lines=file1.readlines()
+    # print read events to console
+    term=' read('
+    term2='tty'
+    log_key(term, term2)
+    # close the file 
+    file1.close()
+else:
+    pass
 
 ## Output E
 print('Starting Output E')
-# output each 'read from keyboard' event
-file_path = 'ist820-eventextraction/DataSource/LogA.strace'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# print read events to console
-term=' read('
-term2='tty'
-term3='pipe'
-log_file(term, term2, term3)
-# close the file 
-file1.close()
+user_input = input('Would you like to perform Output E: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # output each 'read from keyboard' event
+    file_path = 'DataSource/LogA.strace'
+    # open the file
+    file1=open(file_path)
+    # read content into memory object “lines” 
+    lines=file1.readlines()
+    # print read events to console
+    term=' read('
+    term2='tty'
+    term3='pipe'
+    log_file(term, term2, term3)
+    # close the file 
+    file1.close()
+else:
+    pass
 
 ## Output F
 print('Starting Output F')
-# output each 'read from keyboard' event
-file_path = 'ist820-eventextraction/DataSource/LogB.strace'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# print read events to console
-term=' read('
-term2='tty'
-term3='pipe'
-log_file(term, term2, term3)
-# close the file 
-file1.close()
+user_input = input('Would you like to perform Output F: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # output each 'read from keyboard' event
+    file_path = 'DataSource/LogB.strace'
+    # open the file
+    file1=open(file_path)
+    # read content into memory object “lines” 
+    lines=file1.readlines()
+    # print read events to console
+    term=' read('
+    term2='tty'
+    term3='pipe'
+    log_file(term, term2, term3)
+    # close the file 
+    file1.close()
+else:
+    pass
 
 ## Output G
 print('Starting Output G')
 # output a table for each log counting how many times a file was read
-# create a list for file names and counts
+# create a list for file names, counts, timestamps
 file_names = []
 counts = []
-file_path = 'ist820-eventextraction/Outputs/ExerciseA-OutputE.txt'
-# open the file
-file1=open(file_path)
-# read content into memory object “lines” 
-lines=file1.readlines()
-# find file names
-file_name_start = lines.find('<')
-file_name_end = lines.find('>')
-file_name = lines[file_name_start + 1 : file_name_end]
-# add file names to list
-if file_name not in file_names:
-		file_names.append(file_name)
-print(file_names)
-# create file counter
-idx = file_names.index(file_name)
-counts[idx] += 1
-print(f'File Name    |    Counts')
-for i in range(len(file_names)):
-    print(file_name + '\n' + '\n' + counts)
-# close the file 
-file1.close()
+timestamps = []
+file_path1 = 'Outputs/ExerciseA-OutputF.txt'
+file_path2 = 'Outputs/ExerciseB-OutputE.txt'
+user_input = input('Would you like to perform Output G: ')
+user_input = user_input.lower()
+if user_input == 'y' or user_input =='yes':
+    # open the file
+    file1=open(file_path1)
+    file2=open(file_path2)
+    # read content into memory object “lines” 
+    lines1=file1.readlines()
+    lines2=file2.readlines()
+    # find file names
+    if ' read(' in lines and '<' in lines and '>' in lines:
+        file_name_start = lines.find('<')
+        file_name_end = lines.find('>')
+        file_name = lines[file_name_start + 1 : file_name_end]
+        # add file names to list
+        if file_name not in file_names:
+            file_names.append(file_name)
+            counts.append(1)
+            # Starts list with current time.
+            timestamps.append([idx])
+    print(file_names)
+    print(f'File Name    |    Counts')
+    for i in range(len(file_names)):
+        print(file_name + '\t' + '\t' + counts)
+    # close the file 
+    file1.close()
+else:
+    pass
