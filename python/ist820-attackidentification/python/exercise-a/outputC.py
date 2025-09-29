e3ae1034a03812860b8a73fde5b512dbb92055d2
Dani3lyg <86445990+dani3lng@users.noreply.github.com>
2025-09-29 16:57:23 -0700

    in progress

---
+++ python/ist820-attackidentification/python/exercise-a/outputC.py
@@ -0,0 +1,77 @@
+### Exercise A Output C
+
+# variables
+filename = 'ExerciseA-OutputC.txt'
+pids = [None, None, None]
+line_nums = [None, None, None]
+line_strs = [None, None, None]
+kw_found = [False, False, False] 
+
+# pid function
+def pid_from_line(line):
+    idx = line.find(' ')
+    pid = line[0 : idx]
+    return pid
+    
+# establish log path
+file_path = 'data/LogC.strace'
+
+# open the file
+file=open(file_path)
+
+# read content into memory object “lines” 
+lines=file.readlines()
+
+
+# textfile header
+with open(filename, 'a')  as f:
+                f.write("\n******OutputC*****\n\n")
+                f.write("3-stage Event Sequences:\n\n")
+
+# find events in Log X
+line_num = 1
+for line in lines:
+    if 'open(' in line:
+        kw_found[0] = True
+        pids[0] = pid_from_line(line)
+        line_nums[0] = line_num
+        line_strs[0] = line
+    elif 'getdents(' in line:
+        if kw_found[0] == True:
+            
+            # test
+            print('kw found 0 is true')
+            
+            pid = pid_from_line(line)
+            if pid == pids[0]:
+                kw_found[1] = True
+                pids[1] = pid_from_line(line)
+                line_nums[1] = line_num
+                line_strs[1] = line 
+    elif 'close(' in line:
+        if kw_found[1]:
+            pid = pid_from_line(line)
+            if pid == pids[1]:
+                kw_found[2] = True
+                pids[2] = pid_from_line(line)
+                line_nums[2] = line_num
+                line_strs[2] = line
+    if kw_found[0] and kw_found[1] and kw_found[2] == True:
+        print(line_num, line_strs)
+        # # output results to textfile
+        # with open(filename, 'a')  as f:
+        #     f.write("\n******Log X*****\n\n")
+        #     f.write("{:<10} {:<10} \n".format("Timestamp", "Log Entry"))
+        #     f.write("-" * 90  )
+        #     f.write("\n{:<10} {:<10} ".format(line_nums[0], line_strs[0]))
+        #     f.write("{:<10} {:<10} \n".format(line_nums[1], line_strs[1])) 
+        #     f.write("{:<10} {:<10} \n".format(line_nums[2], line_strs[2])) 
+            # clean variables
+    pids = [None, None, None]
+    line_nums = [None, None, None]
+    line_strs = [None, None, None]
+    kw_found = [False, False, False]          
+    line_num += 1       
+# close file
+file.close()
+
---
+++ python/ist820-attackidentification/python/exercise-b/outputA.py
@@ -0,0 +1,64 @@
+### Exercise B Output A
+
+# variables
+filename = 'ExerciseB-OutputA.txt'
+stat_pid = None
+stat_line_num = None
+stat_line = None
+clone_pid = None
+clone_line_num = None
+clone_line = None
+
+# pid function
+def pid_from_line(line):
+    idx = line.find(' ')
+    pid = line[0 : idx]
+    return pid
+
+# establish log path
+file_path = 'data/LogA.strace'
+# open the file
+file=open(file_path)
+# read content into memory object “lines” 
+lines=file.readlines()
+
+# textfile header
+with open(filename, 'a')  as f:
+                f.write("\n******OutputA*****\n\n")
+                f.write("'Stat' and 'Clone' Event Sequences:\n\n")
+                # Format table for output
+                f.write("{:<10} {:<10} \n".format("Timestamp", "Log Entry"))
+                f.write("-" * 90  )
+
+# find 'stat' and 'clone' events
+term1 = ' stat('
+term2 = ' clone('
+line_num = 1
+for line in lines:
+    if term1 in line:
+        stat_pid = pid_from_line(line)
+        stat_line_num = line_num
+        stat_line = line
+    elif term2 in line:
+        clone_pid = pid_from_line(line)
+        clone_line_num = line_num
+        clone_line = line
+        if clone_pid == stat_pid:
+            # output results to console
+            print(str(stat_line_num) + '\t' + stat_line)
+            print(str(clone_line_num) + '\t' + clone_line)
+            # output results to textfile
+            with open(filename, 'a')  as f:
+                f.write("\n{:<10} {:<10} ".format(stat_line_num, stat_line))
+                f.write("{:<10} {:<10} \n".format(clone_line_num, clone_line))
+            # clean variables
+            stat_pid = None
+            stat_line_num = None
+            stat_line = None
+            clone_pid = None
+            clone_line_num = None
+            clone_line = None
+    line_num += 1        
+
+# close file
+file.close()
